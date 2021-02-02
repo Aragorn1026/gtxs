c3577ed351dc09471f8e588fb5a456440b0d0872
Aragorn1026 <294241960@qq.com>
2021-02-02 19:06:06 +0800

    zcq gxts test

--- gtxs-xa-branch/src/main/java/com/example/gtxs/xa/branch/GtxsXaBranchApplication.java
+++ gtxs-xa-branch/src/main/java/com/example/gtxs/xa/branch/GtxsXaBranchApplication.java
@@ -2,8 +2,12 @@ package com.example.gtxs.xa.branch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.gtxs","com.netease.cloud.gtxs"})
+@ImportResource(locations = {"classpath:repository-bean.xml"})
 public class GtxsXaBranchApplication {
 
 	public static void main(String[] args) {
--- gtxs-xa/src/main/java/com/example/gtxs/xa/service/DeptMapperService.java
+++ gtxs-xa/src/main/java/com/example/gtxs/xa/service/DeptMapperService.java
@@ -3,8 +3,10 @@ package com.example.gtxs.xa.service;
 import com.example.gtxs.xa.pojo.Dept;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Transactional
 @Component
 public interface DeptMapperService {
 
