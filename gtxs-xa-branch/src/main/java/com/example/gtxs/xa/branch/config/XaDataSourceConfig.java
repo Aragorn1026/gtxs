62401f366c3f92e360475d8fc54da3b738a1621b
Aragorn1026 <294241960@qq.com>
2021-02-20 15:58:03 +0800

    zcq gxts test;xa-branch20210220,DataSource is changed to Oracle

--- gtxs-xa-branch/src/main/java/com/example/gtxs/xa/branch/config/XaDataSourceConfig.java
+++ gtxs-xa-branch/src/main/java/com/example/gtxs/xa/branch/config/XaDataSourceConfig.java
@@ -7,22 +7,31 @@ import com.mysql.jdbc.jdbc2.optional.MysqlXADataSource;
 import com.netease.cloud.gtxs.core.rm.xa.GtxsXADataSource;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import javax.sql.DataSource;
-import com.mysql.jdbc.jdbc2.optional.MysqlXADataSource;
+import javax.sql.XAConnection;
+
+import oracle.jdbc.xa.client.OracleXADataSource;
+import java.sql.SQLException;
+
 
 @Configuration
 @Component
 public class XaDataSourceConfig {
 
     @Bean
-    public DataSource dataSource(DataSourceProperties dataSourceProperties) {
-        MysqlXADataSource xaDataSource = new MysqlXADataSource();
+    public DataSource dataSource(DataSourceProperties dataSourceProperties) throws SQLException{
+//        mysql数据源
+//        MysqlXADataSource xaDataSource = new MysqlXADataSource();
+
+        // Oracle数据源
+        OracleXADataSource xaDataSource = new OracleXADataSource();
+
         xaDataSource.setURL(dataSourceProperties.getUrl());
         xaDataSource.setUser(dataSourceProperties.getUsername());
         xaDataSource.setPassword(dataSourceProperties.getPassword());
-
         GtxsXADataSource dstpXADataSource = new GtxsXADataSource();
         dstpXADataSource.setTargetXADataSource(xaDataSource);
         dstpXADataSource.setDefault(true);
         return dstpXADataSource;
+
     }
 }
